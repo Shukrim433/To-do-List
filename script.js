83fa32f7ab3e1bafb42774ef308b64ca057934b1
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-10-22 15:34:58 +0100

    added js logic and updated parts of UI

---
+++ script.js
@@ -0,0 +1,24 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li"); // creates li element
+    li.innerHTML = inputBox.value; // sets the text of the li element to input value
+    listContainer.appendChild(li); // (to display the li) append li element to listContainer (ul)
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = ""; // clear input after adding task
+}
+
+listContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") { // if u click a li tag (task), toggles the class "checked"
+    e.target.classList.toggle("checked");
+  } else if (e.target.tagName === "SPAN") { // if u click a span tag ("x"), removes the parent element (task) it is in
+    e.target.parentElement.remove();
+  }
+});
